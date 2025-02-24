6114d81e8447513717a3909b26bccf2c9d8bc12e
Dev Patel <devpatel1828king@gmail.com>
2025-02-24 22:49:15 +0530

    modified socket

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// // index.js
+// const express = require('express');
+// const http = require('http');
+// const socketIo = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIo(server);
+
+// io.on('connection', (socket) => {
+//   console.log('User connected: ' + socket.id);
+  
+//   socket.on('chat message', (msg) => {
+//     console.log('Received message: ' + msg);
+//     io.emit('chat message', msg);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected: ' + socket.id);
+//   });
+// });
+
+// // Bind to all interfaces
+
+
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -24,7 +50,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on http://172.22.240.1:3000');
 });
